47236fcf8b5faf22a94a21f830b792599b4974ff
OneAndZero24 <oneandzero24@gmail.com>
2023-01-26 18:05:57 +0100

    Terms show done

--- src/Structure/Term.hs
+++ src/Structure/Term.hs
@@ -1,4 +1,4 @@
-module Structure.Term(Term(..)) where --TODO
+module Structure.Term(Term(..)) where
 
 -- | Most important structure in program.
 -- Represents λ terms after input is processed by PARSER.
@@ -13,9 +13,9 @@ data Term =
 -- we can use a fixed internal mapping for free variables in output:
 -- a, b, c, ... = 0, 1, 2, ...
 -- | Simply their position in latin alphabet.
--- | Bound variables in output will be outputed as integers.
+-- | Bound variables in output will be outputed as letters prefixed with B.
 
-instance Show Lambda where
+instance Show Term where
     show = (showTerm 0)
 
 -- | Converts λ terms to strings.
@@ -27,24 +27,16 @@ instance Show Lambda where
 -- etc. ...
 showTerm :: Int -> Term -> String
 showTerm i (Var v)
-            | v > i  = idx2char (v-i) -- unbound
-            | v <= i = show v         -- bound
-showTerm i (Abs t) = "(\\"++(concatMap show (bound t))++(showTerm i+1 t)++")"
-showTerm i (App t1 t2) = "("++(showTerm i+1 t1)++" "++(showTerm i+1 t2)++")"
+            | v > i  = [idx2char (v-i)] -- unbound
+            | v <= i = [idx2char (i-v)] -- bound
+showTerm i (Abs t) = "(\\B"++[(idx2char i)]++"."++(showTerm i t)++")"
+showTerm i (App t1 t2) = "("++(showTerm (i+1) t1)++" "++(showTerm (i+1) t2)++")"
 
 -- | Converts char to int. Using it's index in latin alphabet.
 -- | a - 97 in ASCII table.
 char2idx :: Char -> Int
-char2idx = fromEnum.((flip (-)) 97)
+char2idx = ((flip (-)) 97).fromEnum
 
 -- | Does opposite to above.
 idx2char :: Int -> Char
 idx2char = toEnum.((+) 97)
-
--- | Returns list of bound variables in term subtree.
-bound :: Term -> [Int]
--- TODO
-
--- | Collects variables which fullfill given requirement.
-collectVars :: Int -> (Term -> Int -> Bool) -> Term -> [Int]
--- TODO
