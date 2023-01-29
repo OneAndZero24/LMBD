8bc1c7df37a5fbfc872249d933bf3edc8caf0676
OneAndZero24 <oneandzero24@gmail.com>
2023-01-29 23:24:56 +0100

    done :D

--- README.md
+++ README.md
@@ -1,21 +1,46 @@
-# Termy lambda 
-Napisać program, który pobiera ze standardowego wejścia term rachunku lambda i zwraca jego postać normalna, o ile taka istnieje. Nalezy przyjąć, ze termy moga zawierać nawiasy, kropkę, ukosnik (w roli lambdy) oraz zmienne reprezentowane literami z zakresu a–z.
+# **L M B D**
+## Simple λ calculus evaluator/reducer 
+---  
+## Info
+Jan Miksa z1173329  
+Done as Functional Programming course final project.  
+> "Write a program, that takes in λ term from stdio and returns normal form, if such exists. Suppose that λ terms can contain: brackets, dot, slash (used as λ) and variables represented by letters a-z."  
+> "Use only: Prelude, System IO, System Environment"
 
-Prelude, System IO, System Environment
+## How it works?
+This program uses De Bruijn indexing to omit need for alpha reduction. Beta reduction is done in normal order.  
+Such evaluation should result in normal form whenever one exists. Although in `normal` mode it will stop reducing
+once terms reduce to themselves, but in general it can LOOP INFINITELY.
+First `Lexer` processes input into a series of tokens. All of missing brackets omitted due to operator associativity are added.
+Then `Parser` parses these tokens into `Term` tree which is evaluated.
+Parsing in `Parser` and `Lexer` is loosely based on normal order parsing algorithm.
 
-# TODO
+## Compiling
+`ghc --make Main.hs`
 
-6. Desc.
-    - Translate
-    - Algorithms
-    - Analyze complexity
-    - Runing ghc --make
-    - Examples
-    - Full/ Unit Tests
-    - Resources
-    - Sign
+## Running
+`./lmbd *args*`  
+LMBD can run in two modes: REPL or batch.
+In batch mode only argument should be filename to parse. This file can include comments - lines starting with `>`.  
+In REPL mode (activated by `-i` flag) program will query for next terms no reduce. `!` to quit.  
 
-7. Further
-    - Macros
-    - Macro files
+Evaluation can run in 3 modes: `normal`, `greedy`, `one step`.
+- `normal` - default, will reduce until term reduces to itself
+- `greedy` - (activated by `-g` flag), will reduce whenever possible
+- `one step` - (activated by `-s` flag), performs one beta reduction step at a time
+## Examples
+*(aside from ex1.lmbd)*  
+*Bx - means bound variable*
+```
+\>\x.xz \y.xy
+\Ba.((Ba z) (\Bb.(Ba Bb)))
 
+\>(\x.(x y))(\z.z)
+y
+
+\>((\x. \y.(x y))(\y.y)) w
+w
+
+\>(\x.x x) (\y.y x) z
+x x z
+```
--- main.hs
+++ main.hs
@@ -1,5 +1,6 @@
 -- Jan Miksa
 import System.Environment
+import System.IO
 
 import SRC.Structure.Term
 import SRC.Processor.Lexer
@@ -13,42 +14,74 @@ import SRC.Processor.Evaluator
 main :: IO ()
 main = do
     args <- getArgs
-    let e = if elem "-g" args 
-        then eval
-        else if elem "-s" args
-            then reduce
-            else reduce2Norm
-    if elem "-i" args
-        then (repl e)
-        else (fileE e (head (filterFileName args)))
+    if (length args) /= 0
+        then do
+            let e = if elem "-g" args 
+                then (eval)
+                else if elem "-s" args
+                    then (reduce)
+                    else (reduce2Norm)
+            if elem "-i" args
+                then do 
+                    repl e
+                    return ()
+                else do (fileE e (head (filterFileName args)))
+        else do
+            putStrLn("      ___       ___           ___           ___")       
+            putStrLn("     /\\__\\     /\\__\\         /\\  \\         /\\  \\")  
+            putStrLn("    /:/  /    /::|  |       /::\\  \\       /::\\  \\")  
+            putStrLn("   /:/  /    /:|:|  |      /:/\\:\\  \\     /:/\\:\\  \\") 
+            putStrLn("  /:/  /    /:/|:|__|__   /::\\~\\:\\__\\   /:/  \\:\\__\\") 
+            putStrLn(" /:/__/    /:/ |::::\\__\\ /:/\\:\\ \\:|__| /:/__/ \\:|__|")
+            putStrLn(" \\:\\  \\    \\/__/~~/:/  / \\:\\~\\:\\/:/  / \\:\\  \\ /:/  /")
+            putStrLn("  \\:\\  \\         /:/  /   \\:\\ \\::\\  \\   \\:\\  /:/  /")
+            putStrLn("   \\:\\  \\       /:/  /     \\:\\/:/  /     \\:\\/:/  /")
+            putStrLn("    \\:\\__\\     /:/  /       \\::/  /       \\::/  /")
+            putStrLn("     \\/__/     \\/__/         \\/__/         \\/__/")  
+            putStrLn("---------------------------------- v.0.0.1 Jan Miksa-")
+            putStrLn("| LMBD - Lambda expression evaluator/reducer.       |")
+            putStrLn("| Usage:                                            |")
+            putStrLn("| - lmbd \"test.lmbd\" - run file                     |") 
+            putStrLn("| - [-i] - REPL                                     |")
+            putStrLn("| - [-g] - greedy evaluation mode                   |")
+            putStrLn("| - [-s] - perform only one beta reduction step     |")
+            putStrLn("| - ! - quit interactive mode                       |")
+            putStrLn("-----------------------------------------------------")      
 
 -- | Filter filename from arguments
 filterFileName :: [String] -> [String]
 filterFileName s = filter (\x -> not (elem x ["-i", "-s", "-g"])) s
 
+-- | REPL prompt
+prompt :: String -> IO String
+prompt t = do
+    putStr t
+    hFlush stdout
+    getLine
+
 -- | REPL loop mode
 -- Takes in evaluator as argument
 repl :: (Term -> Term) -> IO ()
 repl f = do
-    putStr "\\>"
-    l <- getLine
-    case (head l) of
-        '>' -> do repl f   -- comment
-        '!' -> return ()   -- exit
-        otherwise -> do
-            case (tokenize l) of
-                Nothing -> do
-                    putStrLn "Syntax error!"
-                    repl f
-                (Just t) -> do
-                    if not e
-                        then do
-                            putStrLn "Syntax error!"
-                            repl f
-                        else do 
-                            print (f x)
-                            repl f
-                    where (x, e) = parse t
+    l <- (prompt "\\>")
+    if l == [] then do repl f
+        else case (head l) of
+            '>' -> do repl f   -- comment
+            '!' -> return ()   -- exit
+            otherwise -> do
+                case (tokenize l) of
+                    Nothing -> do
+                        putStrLn "Syntax error!"
+                        repl f
+                    (Just t) -> do
+                        if not e
+                            then do
+                                putStrLn "Syntax error!"
+                                repl f
+                            else do 
+                                print (f x)
+                                repl f
+                        where (x, e) = parse t
 
 -- | File input mode
 fileE :: (Term -> Term) -> String -> IO ()
